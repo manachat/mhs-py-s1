c799ae78247c381f039464d0d50d7e9c933e93a9
vafilonov <sev.filonov@gmail.com>
2023-12-10 13:04:04 +0300

    hw 2

--- hw_2/hw_2_2.py
+++ hw_2/hw_2_2.py
@@ -3,6 +3,7 @@ from hw_2_1 import latex_table
 import subprocess
 
 __doc_template = """\\documentclass{{article}}
+\\usepackage{{graphicx}}
 \\begin{{document}}
 {contents}
 \\end{{document}}"""
@@ -26,10 +27,9 @@ def main(filename):
        contents = latex_table([[10, 20, 30], ['i', 'love', 'cats']]) + latex_pic('artifacts/random_cat.jpeg')
        out.write(__doc_template.format_map({'contents': contents}))
 
-   cmd = f"pdflatex -interaction nonstopmode -output-directory=artifacts/ {tmp_file}"
-   res = subprocess.run(cmd)
-   #pdfl = pl.PDFLaTeX.from_texfile('artifacts/example.txt')
-   #pdf, log, completed_process = pdfl.create_pdf(keep_pdf_file=True, keep_log_file=True)
+   cmd = f"pdflatex -interaction nonstopmode -output-directory=out/ {tmp_file}"
+   res = subprocess.run(cmd, shell=True)
+
 
 if __name__ == '__main__':
     main()
